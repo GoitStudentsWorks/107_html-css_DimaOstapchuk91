9a24811e5341d6ee6849857feb74be7d71983c70
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-05 11:45:30 +0300

    add js swiper

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 import './js/menu.js';
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1280: {
+      loop: false,
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
+});
